9d962ae15eca78b49db298acfa3a394df7f1b92f
gsilver7 <157014675+gsilver7@users.noreply.github.com>
2025-05-12 20:55:16 +0900

    2.0.0
    주석 옮기고 작업 시작

--- mode_drive.py
+++ mode_drive.py
@@ -5,80 +5,86 @@ import signal
 import numpy as np
 import cv2
 from math import inf
- 
+
 # 이 코드(class)는 step함수에서 다음과 같이 동작한다.
 # 1. 이미지(카메라 데이터)와 class 내의 state를 기반으로 차선의 위치를 알아낸다.
 #   1.1. 이미지를 항공뷰로 변환한다.
-# 2. 차선의 위치를 기반으로 position을 구한다. 
+# 2. 차선의 위치를 기반으로 position을 구한다.
 # 3. p제어를 하여 앞바퀴의 각도를 반환한다. 속도는 임의로 설정한다.
 
 
 class Drive:
     from enum import Enum
 
-    class State(Enum):
-        DRIVE_STATE_NONE = 0
-        DRIVE_STATE_LEFT = 1
-        DRIVE_STATE_RIGHT = 2
+    class State(Enum):            # 주행 모드 / 구조체 처럼 사용할 변수들
+        DRIVE_STATE_NONE = 0      # 직선 주행
+        DRIVE_STATE_LEFT = 1      # 좌회전
+        DRIVE_STATE_RIGHT = 2     # 우회전
 
-    def __init__(self, image, ranges) -> None:
-        """
-        state class를 초기화하는 함수. 관련 상수는 다 여기서 고치면 된다.
-        """
-        self._image = image
-        self._ranges = ranges
+    def __init__(self, image, ranges) -> None:       # state class를 초기화하는 함수 / 관련 상수 정의
+        
+        self._image = image          # 받아온 센서 값
+        self._ranges = ranges        # 받아온 주행 관련 값 (속도, 조향 ..)
 
-        # ================== [ 영상 처리 관련 변수 시작 ] ==================
-
-
+        
         # 카메라 이미지의 크기를 나타내는 상수
-        self._camera_width = 640  # 높이 픽셀 수
-        self._camera_height = 480  # 너비 픽셀 수
-        # 연산량을 줄이기 위해 이미지를 줄였을 때의 크기를 나타내는 상수
-        # 이때 원본 이미지 크기의 높이와 너비에 비해 각각 8배 작다.
-        self._camera_width_s = self._camera_width // 8  # 높이 픽셀 수
-        self._camera_height_s = self._camera_height // 8  # 너비 픽셀 수
+        self._camera_width = 640     # 높이 픽셀 수
+        self._camera_height = 480    # 너비 픽셀 수
 
-        # perspective matrix 생성
-        self._persptrans_matrix = self.__get_persptrans_matrix()
+        
+        
+        
+        # 이미지를 8배 축소할때 쓸 계수 / 연산량 감소를 위해
+        self._camera_width_s = self._camera_width // 8      # 높이 픽셀 수
+        self._camera_height_s = self._camera_height // 8    # 너비 픽셀 수
 
-        # 흰색 픽셀을 검출할 때의 민감도 설정
-        self._white_sensitivity = 30
-        # base line ratio는 기준선을 화면의 어느 지점으로 정할지를 결정하는 비율이다.
-        # 0.8이면, 이미지의 높이가 1일 때 0.2의 높이를 가지는 선이 기준선이 되는 것이다.
-        self._base_line_ratio = 0.8
-        self._base_line_position = int(self._camera_height_s * self._base_line_ratio)
 
+
+        
+        
+        self._persptrans_matrix = self.__get_persptrans_matrix()      # perspective matrix 생성 / 아래 정의된 메소드 사용
+
+        
+        
+        self._white_sensitivity = 30      # 흰색 픽셀을 검출할 때의 민감도 설정 like threshold
+
+        
+        
+        
+        self._base_line_ratio = 0.8                                                      # 기준선을 화면의 어느 지점으로 정할지를 결정하는 비율
+        self._base_line_position = int(self._camera_height_s * self._base_line_ratio)    # 비율을 실제 높이로 변환한 값
+        
+        
         ### 차선 판단 관련 상수 ###
-        # base line정보의 길이 설정
-        self._base_line_data_length = self._camera_width_s
-        # 왼쪽 차선의 초기값 설정
-        self._pos_l = 0
-        # 오른쪽 차선의 초기값 설정
-        self._pos_r = self._base_line_data_length - 1
-        # position(차선의 가운데에서 벗어난 정도)초기값 설정
-        self._pos = 0
-        # 직진 구간 판단 범위 설정
-        self._center_range = 20
-        # 스테이트를 저장하는 변수의 초기값 설정
-        self._state = self.State.DRIVE_STATE_NONE
-        # 일반적인 차선의 폭 설정
-        self._lane_width = 60
-        # 차선 폭의 최솟값 설정
-        self._lane_width_min = 24
-        # 점수 임계치가 가지는 하한선 설정
-        self._score_bound_min = -24
+        
+        self._base_line_data_length = self._camera_width_s  # base line정보의 길이 설정
+        
+        self._pos_l = 0                                     # 왼쪽 차선의 초기값 설정
+       
+        self._pos_r = self._base_line_data_length - 1       # 오른쪽 차선의 초기값 설정
+        
+        self._pos = 0                                       # position(차선의 가운데에서 벗어난 정도)초기값 설정
+       
+        self._center_range = 20                             # 직진 구간 판단 범위 설정
+        
+        self._state = self.State.DRIVE_STATE_NONE           # 스테이트를 저장하는 변수의 초기값 설정
+       
+        self._lane_width = 60                               # 일반적인 차선의 폭 설정
+        
+        self._lane_width_min = 24                           # 차선 폭의 최솟값 설정
+       
+        self._score_bound_min = -24                         # 점수 임계치가 가지는 하한선 설정
 
+        
         ### 각도, 속도 관련 상수 ###
         self._p_con_p = 2
         self._speed = 5
         self._curve_decel = 5
 
-        # ================== [ 영상 처리 관련 변수 끝 ] ==================
-
         # Logging
-        self._enable_logging = True
+        self._enable_logging = True  # true면 로깅 함 / 로깅은 디버깅 비슷한 느낌
 
+    
     def __get_persptrans_matrix(self):
         """
         cv2.warpPerspective() 함수에 사용할 Perspective Transform Matrix를 반환하는 함수.\n
@@ -138,50 +144,61 @@ class Drive:
 
         return cv2.getPerspectiveTransform(src, dst)
 
+
+
+
+    
+
     def __detect_lane(self, image):
         """
         이 함수는 base line(기준선)에 감지된 lane으로 추정되는 흰색 픽셀들의 위치를 반환하는 함수이다.\n
         예를 들어 base line의 데이터 길이가 20이라면, 아래와 같은 출력이 나오는 것이다.\n
         -> 01000,00010,00000,10000
         """
-        # perspective transform : 항공뷰 구하기
-        img = cv2.warpPerspective(
-            image,
-            self._persptrans_matrix,
-            (self._camera_width, self._camera_height),
+        
+        img = cv2.warpPerspective(                                      # perspective transform : 항공뷰 구하기 / 선형변환 느낌
+            image,                                                      # 원본
+            self._persptrans_matrix,                                    # 변환 행렬 init 할때 구함
+            (self._camera_width, self._camera_height),                  # 카메라 이미지 크기 / init에서 정의됨
         )
 
-        # resize : 이미지 크기 줄이기 - 연산량 감소를 위해
-        img_small = cv2.resize(
-            img,
-            dsize=(self._camera_width_s, self._camera_height_s),
-            interpolation=cv2.INTER_NEAREST,
+       
+        img_small = cv2.resize(                                         # resize : 이미지 크기 줄이기 - 연산량 감소를 위해
+            img,                                                        # 원본
+            dsize=(self._camera_width_s, self._camera_height_s),        # 변환 후 크기 / init에서 정의됨
+            interpolation=cv2.INTER_NEAREST,                            # 보간 방법 / 몰라도 될듯
         )
 
-        # cvtColor : 색공간 변환(BGR -> HSV)
-        img_hsv = cv2.cvtColor(img_small, cv2.COLOR_BGR2HSV)
+        
+        img_hsv = cv2.cvtColor(img_small, cv2.COLOR_BGR2HSV)             # cvtColor : 색공간 변환(BGR -> HSV)
+        
 
-        # inRange: 차선 색만 검출. 이때 차선 색은 흰색이라 가정한다.
-        # 따라서 흰색 픽셀만 검출한다.
-        lower_white = np.array([0, 0, 255 - self._white_sensitivity])
-        upper_white = np.array([255, self._white_sensitivity, 255])
-        img_white = cv2.inRange(img_hsv, lower_white, upper_white)
+                                                                         # 흰색만 검출하므로 수정 필요
+        lower_white = np.array([0, 0, 255 - self._white_sensitivity])    # 판단 하한
+        upper_white = np.array([255, self._white_sensitivity, 255])      # 판단 상한
+        img_white = cv2.inRange(img_hsv, lower_white, upper_white)       # 범위에 해당되면 1을 반환 / 흰색이라고 판단
 
-        # 기준선(base line)위에서 차선이 존재하는 위치만 1이다.
-        base_line = img_white[self._base_line_position, :]
+        
+        base_line = img_white[self._base_line_position, :]               # 기준선(base line)위에서 차선이 존재하는 위치만 1로 반환 (01000,00010,00000,10000)
 
-        self._logging(img_original=img_white)
+        self._logging(img_original=img_white)                            # logging 메소드 실행 / 디버그 느낌
 
         return base_line
 
-    def _logging(self, img_original):
-        if not self._enable_logging:
+
+
+    
+
+    def _logging(self, img_original):                                  # 로깅 함수
+        
+        if not self._enable_logging:                                   # 로깅 꺼져있으면 바로 탈출 / 개발 끝나면 false로 설정함
             return
 
-        # lane 정보를 기준선과 함께 화면에 띄운다.
-        img_lane = img_original.copy()  #
-        img_lane = cv2.cvtColor(img_lane, cv2.COLOR_GRAY2BGR)
-        img_lane = cv2.line(
+
+                                                                   # lane 정보를 기준선과 함께 화면에 띄운다.     
+        img_lane = img_original.copy()                             # 인자로 받은 img를 img_lane에 복사함
+        img_lane = cv2.cvtColor(img_lane, cv2.COLOR_GRAY2BGR)      # 흑백 이미지를 BGR 이미지로 변환 / 함수에서 써먹기위해
+        img_lane = cv2.line(                                        
             img_lane,
             (0, self._base_line_position),
             (self._camera_width_s - 1, self._base_line_position),
@@ -212,8 +229,10 @@ class Drive:
 
     def __choose_lane(self, positions):
         """
-        lane으로 추정될 수 있는 데이터들을 가져와, 실제 lane은 무엇인지 판단하고
-        차선의 중앙에서 얼마나 떨어져있는지(position) 반환하는 함수이다.\n
+        1. lane으로 추정될 수 있는 데이터들을 가져옴
+        2. 실제 lane은 무엇인지 판단함
+        3. 차선의 중앙에서 얼마나 떨어져있는지(position) 반환
+        
 
         이 함수는 y=-abs(curr_x - prev_x) 휴리스틱 함수를 이용하여 검출된 차선 중
         이전에 결정된 차선에 가장 가까운 차선의 위치를 고른다.\n
@@ -221,6 +240,7 @@ class Drive:
         초기에는 먼 거리에 존재하는 차선도 인식시키기 위해(차량이 보고 있는 차선은 매번 다르므로)
         하한선 개념을 도입하여 차선이 검출될 때마다 하한선을 조금식 올리며 점차 그 간극을 줄여나간다. \n
         """
+        
         # score는 가능한 가장 낮은 점수의 값으로 설정
         score_l, pos_l = -inf, None
         score_r, pos_r = -inf, None
@@ -296,7 +316,8 @@ class Drive:
 
         return self._pos
 
-    def step(self):
+    def step(self):                                             # main 함수에서 실행할 동작
+        
         image = self._image
 
         lane_candidates = self.__detect_lane(image)
@@ -305,5 +326,5 @@ class Drive:
 
         angle = self._p_con_p * position
 
-        return angle, speed
+        return angle, speed              # 곡선 감속 추가 안 함
         
