9cd4c2cfdd83a8165b65d8f7a743d7a09e16f325
DelionMin <134771247+DelionMin@users.noreply.github.com>
2025-05-16 17:15:13 +0900

    Create mode_rubbercone.py
    Commited initially

---
+++ mode_rubbercone.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Rubbercone:
+
+    def __init__(self) -> None:
+        self._image = None
+        self._ranges = None
+
+    def get_value(self, image, ranges):
+        # 센서 값 받아오기 위한 메소드
+        self._image = image
+        self._ranges = ranges 
+
+    def step():
+        ranges = self._ranges
+        # tuple 자료형, 정면을 기준으로 0번 ~ 반시계 방향
+        # tuple 내 요소들은 최댓값 100 -> 측정 최대치가 100이라 사실 값이 100인 각도는 아무것도 안 걸린 걸로 봐야한다
+
+        '''
+        내용 채우기 
+        '''
+
+        return angle, speed
